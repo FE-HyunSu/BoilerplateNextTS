d824ecdac7d5248e9a04dc5e91e8e789a6a69c09
KHS <hyunsusora@gmail.com>
2022-11-21 22:00:27 +0900

    add, modify, delete : file set.

---
+++ components/layout/header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderBox } from './style';
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBox>
+        <h1>
+          <strong>🥸 React Study Group</strong>
+          <a href="https://reliable-florentine-21f16f.netlify.app">ADMIN</a>
+        </h1>
+      </HeaderBox>
+    </>
+  );
+};
+export default Header;
